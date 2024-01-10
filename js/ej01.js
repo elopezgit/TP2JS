e1ccacbbc0eb27850d134f64f4c08ef0f03b4c4e
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-01-10 18:26:06 -0300

    creamos la estructura del proyecto

---
+++ js/ej01.js
@@ -0,0 +1,46 @@
+
+// Ejercicio 1: Variables y Operaciones Matemáticas
+
+// Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, 
+//realiza las siguientes operaciones y muestra los resultados en la pantalla:
+
+// Suma de numero1 y numero2.
+
+// Resta de numero1 y numero2.
+
+// Multiplicación de numero1 y numero2.
+
+// División de numero1 entre numero2.
+
+
+//mostrar un mensaje en el documento HTML
+document.write('<p class = "text-center text-primary">Trabajo Practico Numero 1 - EJERCICIO 1 - Variables y Operaciones Matemáticas</p>')
+
+
+
+let numero1 = 500
+let numero2 = 400
+
+document.write('<br>' + 'Suma de numero1 y numero2:')
+document.write('<br>' + 'Numero 1 es: ' + numero1)
+document.write('<br>' + 'Numero 2 es: ' + numero2)
+document.write('<br>' + 'Resultado es: ' + (numero1+numero2))
+document.write('<br>')
+
+document.write('<br>' + 'Resta de numero1 y numero2.')
+document.write('<br>' + 'Numero 1 es: ' + numero1)
+document.write('<br>' + 'Numero 2 es: ' + numero2)
+document.write('<br>' + 'Resultado es: ' + (numero1-numero2))
+document.write('<br>')
+
+document.write('<br>' + 'Multiplicación de numero1 y numero2.')
+document.write('<br>' + 'Numero 1 es: ' + numero1)
+document.write('<br>' + 'Numero 2 es: ' + numero2)
+document.write('<br>' + 'Resultado es: ' + (numero1*numero2))
+document.write('<br>')
+
+document.write('<br>' + 'División de numero1 entre numero2.')
+document.write('<br>' + 'Numero 1 es: ' + numero1)
+document.write('<br>' + 'Numero 2 es: ' + numero2)
+document.write('<br>' + 'Resultado es: ' + (numero1/numero2))
+document.write('<br>')
