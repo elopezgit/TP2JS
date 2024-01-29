8ab1ce19630a160682d48146c96904cf5ed9d966
Esteban Lopez <lopezestebanalejandro@gmail.com>
2024-01-29 17:06:13 -0300

    tp2ej13 pide texto lo muestra en mayuscula

---
+++ js/ej13.js
@@ -0,0 +1,10 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+// Pedir al usuario que ingrese un texto
+let texto = prompt("Ingrese un texto:");
+
+// Convertir el texto a mayúsculas
+let textoEnMayusculas = texto.toUpperCase();
+
+// Mostrar el texto en mayúsculas
+document.write("Texto en mayúsculas: " + textoEnMayusculas);
